1b3164a299dc1d1bea53fd81fff7b1b94c0f1116
Eriche Gonzales <erichelouise@gmail.com>
2022-09-01 13:28:32 -0400

    changed events database to add images

--- cosmo-client/src/components/Event.js
+++ cosmo-client/src/components/Event.js
@@ -2,10 +2,10 @@ import { useNavigate } from "react-router-dom";
 
 const Event = ({ event, setEventChosen }) => {
   const navigate = useNavigate();
-  const banner = event.images[0].url;
-  const event_picture = event.images[1].url;
-  const event_picture_2 = event.images[2].url;
-  const { name, description, game, time_start, time_end, ticket_price } = event;
+  // const banner = event.images[0].url;
+  // const event_picture = event.images[1].url;
+  // const event_picture_2 = event.images[2].url;
+  const { name, description, game, time_start, time_end, ticket_price, banner, image_1, image_2 } = event;
 
   const handleEventChosen = () => {
     setEventChosen({
@@ -23,8 +23,8 @@ const Event = ({ event, setEventChosen }) => {
     <li className="event-cards">
       <div className="card">
         <img className="banner" src={banner} alt={name} />
-        <img className="card-image1" src={event_picture} alt={name} />
-        <img className="card-image2" src={event_picture_2} alt={name} />
+        <img className="card-image1" src={image_1} alt={name} />
+        <img className="card-image2" src={image_2} alt={name} />
         <div className="card_content">
           <div className="card_title">{name}</div>
           <p className="card_text">{description}</p>
